0317373902d6a48dde77bfc2718191a5a223563f
Vasu Vanka <vasu@olivetech.net>
2018-02-20 15:08:33 +0530

    added documentation

--- core/logger.js
+++ core/logger.js
@@ -6,6 +6,7 @@
 const chalk = require('chalk');
 const {Console} = require('console');
 const logger = new Console(process.stdout, process.stderr);
+
 /**
  * @ignore
  * To get chalked text
@@ -13,16 +14,17 @@ const logger = new Console(process.stdout, process.stderr);
  * @param color - required color
  * @returns {*} - chalked text
  */
-const getChalkText = (text, color) => {
+function getChalkText(text, color) {
     return chalk[color](text);
 };
+
 /**
  * @ignore
  * To log into stdout
  * @param message - log text
  * @param kind - type of log. ex: log,error,info,warning
  */
-const log = (message, kind = 'log') => {
+async function log(message, kind = 'log') {
     if (typeof message !== 'string') {
         if (typeof message === 'object' && message !== null) {
             message = JSON.stringify(message);
@@ -34,20 +36,25 @@ const log = (message, kind = 'log') => {
         logger.log({type: kind, message: message})
     }
 };
+
 /**
  * @ignore
  * To log a plain text
  * @param message - log text
  */
-const plainLog = (message) => {
+async function plainLog(message) {
     logger.log(message);
 }
+
 /**
  * To log user text as json object
  * @param message - log text
  * @param type - type of log. ex: log,error,info,warning
  * @example
  * logger.log('hello world', 'log');
+ * @example
+ * //log output
+ * { type: 'log', message: 'hello world' }
  */
 module.exports.log = (message, type = 'log') => log(`${message}`, type);
 /**
@@ -89,7 +96,22 @@ module.exports.warning = (message) => {
 /**
  * To log text with given rgb color code or color name
  * @param message - log text
- * @param color - color ex: all chalk supported colors
+ * @param color - color [ black,
+ * red,
+ * green,
+ * yellow,
+ * blue,
+ * magenta,
+ * cyan,
+ * white,
+ * gray ("bright black"),
+ * redBright,
+ * greenBright,
+ * yellowBright,
+ * blueBright,
+ * magentaBright,
+ * yanBright,
+ * whiteBright ]
  * @example
  * logger.customLog('hello world','magentaBright');
  */
